a79923c244970a1e493ca39dcbbe20e2fdc4f528
Modris Ābrams <brainstormer57@googlemail.com>
2019-05-14 20:01:36 +0300

    olmpyia

--- app/src/main/java/com/example/modris/sendtest2/MainActivity.java
+++ app/src/main/java/com/example/modris/sendtest2/MainActivity.java
@@ -4,25 +4,38 @@ import android.content.Intent;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    SendEmail sm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        Log.d("main", " 1");
+
+//        Intent ok = new Intent(this, MainActivity.class);
+
+
+        sm = new SendEmail("brainstormer57@gmail.com", "Test",
+                "this is a default message");
         setContentView(R.layout.activity_main);
         EditText Message  = (EditText)findViewById(R.id.txtMsg);
         Button btn = (Button)findViewById(R.id.btnSend);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SendEmail.Send();
+                sm.Send();
 
             }
 
         });
     }
 }
+
+
+
+// objekts on create
--- app/src/main/java/com/example/modris/sendtest2/SendEmail.java
+++ app/src/main/java/com/example/modris/sendtest2/SendEmail.java
@@ -2,35 +2,31 @@ package com.example.modris.sendtest2;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import com.example.modris.sendtest2.R;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.util.Log;
 
- class SendEmail extends AppCompatActivity {
 
-    String[] To;
+ public class SendEmail extends AppCompatActivity {
+
+    private String To;
     String Subject;
     String Message;
+//    private Intent ok;
 
-    SendEmail(String To, String Subject, String Message) {
+    public SendEmail(String To, String Subject, String Message) {
 
-         this.To[1] = To;
+         this.To= To;
          this.Subject = Subject;
          this.Message = Message;
+//         this.ok = ok;
 
     }
-    public static void Send() {
-        SendEmail sm = new SendEmail("brainstormer57@gmail.com", "Test",
-                "this is a default message");
-                Intent ok = new Intent(Intent.ACTION_SEND);
-               ok.putExtra(Intent.EXTRA_EMAIL, sm.To);
-                ok.putExtra(Intent.EXTRA_SUBJECT,sm.Subject);
-               ok.putExtra(Intent.EXTRA_TEXT, sm.Message);
+    public void Send() {
+               Intent ok = new Intent(Intent.ACTION_SEND);
+               ok.putExtra(Intent.EXTRA_EMAIL, new String[] { To });
+                ok.putExtra(Intent.EXTRA_SUBJECT,Subject);
+               ok.putExtra(Intent.EXTRA_TEXT, Message);
                 ok.setType("message/rfc822");
-//        startActivity(Intent.createChooser(ok, "Choose Mail App"));
+        startActivity(ok);
     }
 }
 
